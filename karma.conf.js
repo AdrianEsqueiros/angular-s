654c723eaeffb2bcc03644c6951dedd5d92da75b
Adrian Esqueiros <u291718169@upc.edu.pe>
2022-11-21 09:39:19 -0500

    master

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/learning-center'),
+      dir: require('path').join(__dirname, './coverage/catalogue'),
       subdir: '.',
       reporters: [
         { type: 'html' },
